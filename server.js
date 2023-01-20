1ff805f754769ba11f287aa97b0f519cb31211a2
Nadav Vilder <nadav.vilder@siemens.com>
2023-01-20 19:13:13 +0200

    port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const fs = require("fs");
 
 app.use(
